c6c8fa2093d9f4586bc339b8909336a4520ba809
ConvexBurrito5 <ConvexBurrito5@gmail.com>
2025-05-07 04:22:49 -0400

    uipatret

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ VENV_DIR="$PROJECT_ROOT/.venv"
 source "$VENV_DIR/bin/activate"
 
 cd "$PROJECT_ROOT/Codebase/FastAPIapp"
-echo "Running FastAPI from $(pwd)"
+export PYTHONPATH=".:$PYTHONPATH"
 
+echo "Running FastAPI from $(pwd)"
 exec uvicorn main:app --host 0.0.0.0 --port 8000
